86c3b7109fafd4afcd4be7c15bd463d5f5c63347
Riley Huppatz <huprc001@mymail.unisa.edu.au>
2018-02-14 17:31:16 +1100

    Gate can open

---
+++ Assets/OpenGate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour {
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown("space") && anim.GetBool("isOpen") == false)
+        {
+            anim.SetBool("isOpen", true);
+        }
+    else
+        {
+            anim.SetBool("isOpen", false);
+        }
+	}
+}
