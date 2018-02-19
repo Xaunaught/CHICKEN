99ff709f36241dae649187fef002e6c93c87e77b
Riley Huppatz <huprc001@mymail.unisa.edu.au>
2018-02-19 17:03:41 +1100

    UtilAI base set up

---
+++ Assets/Scripts/UtilAI/Hunger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger : AIAction {
+
+	public override float Evaluate(AnimalAgent agent)
+	{
+		float evaluation = agent.Hunger;
+		return evaluation;
+	}
+
+	public override void UpdateAction(AnimalAgent agents)
+	{
+		agents.NavMeshAgent.SetDestination(agents.FoodSource.transform.position);
+	}
+}
