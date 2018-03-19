5ab3fb439324495c9aada196dfa415663df99982
Riley Huppatz <huprc001@mymail.unisa.edu.au>
2018-03-19 16:22:34 +1030

    Fixed open gate

---
+++ Assets/Scripts/TriggerAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TriggerAction : VRTK_InteractableObject {
+
+	private Animator anim;
+	
+	public override void StartUsing(VRTK_InteractUse usingObject)
+	{
+		base.StartUsing(usingObject);
+		PlayAnim();
+	}
+	
+	public override void StopUsing(VRTK_InteractUse usingObject)
+	{
+		base.StopUsing(usingObject);
+	}
+	
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void PlayAnim()
+	{
+		if (anim.GetBool("isOpen") == false)
+		{
+			anim.SetBool("isOpen", true);
+		}
+		else if (anim.GetBool("isOpen") == true)
+		{
+			anim.SetBool("isOpen", false);
+		}
+	}
+}
